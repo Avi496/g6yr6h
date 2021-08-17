581a1028a984a06da1f7e91b9add7b6804340147
Avi496 <78342708+Avi496@users.noreply.github.com>
2021-08-17 17:06:00 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,13 @@
+function preload(){
+
+}
+function setup(){
+    canvas=createCanvas(360, 300);
+    canvas.center();
+}
+function draw(){
+
+}
+function ApplyFilter(){
+    save("sillyIMG.png");
+}
